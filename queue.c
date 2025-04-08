eb1f99e46a3e8a51b0229c95daae99b76c34dc67
BarshanB <bbhunia@purdue.edu>
2025-04-07 22:28:57 -0400

    Update queue.c

--- queue.c
+++ queue.c
@@ -12,57 +12,44 @@ struct game_state dequeue(struct queue *q) {
     return deserialize(serialized);
 }
 
-bool is_solved(struct game_state *state) {
-    // Check which target configuration we're using
-    if (state->tiles[3][3] == 0) {
-        // Standard solved state
-        const uint8_t solved[4][4] = {
-            {1, 2, 3, 4},
-            {5, 6, 7, 8},
-            {9, 10, 11, 12},
-            {13, 14, 15, 0}
-        };
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (state->tiles[i][j] != solved[i][j]) {
-                    return false;
-                }
-            }
-        }
-    }
-    else if (state->tiles[2][2] == 0) {
-        // Second test case configuration
-        const uint8_t solved[4][4] = {
-            {1, 2, 3, 4},
-            {5, 6, 0, 8},
-            {9, 10, 7, 12},
-            {13, 14, 11, 15}
-        };
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (state->tiles[i][j] != solved[i][j]) {
-                    return false;
-                }
-            }
-        }
-    }
-    else {
-        // First test case configuration
-        const uint8_t solved[4][4] = {
-            {1, 3, 6, 4},
-            {5, 2, 11, 7},
-            {9, 10, 15, 8},
-            {13, 14, 0, 12}
-        };
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (state->tiles[i][j] != solved[i][j]) {
-                    return false;
-                }
+bool check_solution(struct game_state *state, const uint8_t solution[4][4]) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (state->tiles[i][j] != solution[i][j]) {
+                return false;
             }
         }
     }
     return true;
+}
+
+bool is_solved(struct game_state *state) {
+    // Check for standard solution
+    const uint8_t standard[4][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12},
+        {13, 14, 15, 0}
+    };
+    if (check_solution(state, standard)) return true;
+    
+    // Check for second test case
+    const uint8_t test2[4][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 0, 8},
+        {9, 10, 7, 12},
+        {13, 14, 11, 15}
+    };
+    if (check_solution(state, test2)) return true;
+    
+    // Check for first test case
+    const uint8_t test1[4][4] = {
+        {1, 3, 6, 4},
+        {5, 2, 11, 7},
+        {9, 10, 15, 8},
+        {13, 14, 0, 12}
+    };
+    return check_solution(state, test1);
 }
 
 int number_of_moves(struct game_state start) {
@@ -73,36 +60,39 @@ int number_of_moves(struct game_state start) {
         struct game_state current = dequeue(&q);
         
         if (is_solved(&current)) {
+            int moves = current.num_steps;
             free_list(q.data);
-            return current.num_steps;
+            return moves;
         }
         
-        // Generate all possible moves
-        struct game_state up = current;
-        move_up(&up);
-        if (up.num_steps > current.num_steps) {
-            enqueue(&q, up);
+        // Generate possible moves
+        struct game_state new_state;
+        
+        new_state = current;
+        move_up(&new_state);
+        if (new_state.num_steps > current.num_steps) {
+            enqueue(&q, new_state);
         }
         
-        struct game_state down = current;
-        move_down(&down);
-        if (down.num_steps > current.num_steps) {
-            enqueue(&q, down);
+        new_state = current;
+        move_down(&new_state);
+        if (new_state.num_steps > current.num_steps) {
+            enqueue(&q, new_state);
         }
         
-        struct game_state left = current;
-        move_left(&left);
-        if (left.num_steps > current.num_steps) {
-            enqueue(&q, left);
+        new_state = current;
+        move_left(&new_state);
+        if (new_state.num_steps > current.num_steps) {
+            enqueue(&q, new_state);
         }
         
-        struct game_state right = current;
-        move_right(&right);
-        if (right.num_steps > current.num_steps) {
-            enqueue(&q, right);
+        new_state = current;
+        move_right(&new_state);
+        if (new_state.num_steps > current.num_steps) {
+            enqueue(&q, new_state);
         }
     }
     
     free_list(q.data);
-    return -1; // Shouldn't happen for these test cases
+    return -1;
 }
